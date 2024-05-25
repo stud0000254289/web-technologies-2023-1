9837ab4714ac2d9a958875eb28f0719fa9a9b9f8
stud0000254289 <115467419+stud0000254289@users.noreply.github.com>
2024-05-25 08:46:55 +0500

    Update database.sql

--- db/database.sql
+++ db/database.sql
@@ -1,24 +1,24 @@
 CREATE TABLE menu_items (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    parent_id INT DEFAULT NULL,
-    has_children BOOLEAN NOT NULL DEFAULT 0
+    parent_id INT DEFAULT NULL
 );
 
-INSERT INTO menu_items (name, parent_id, has_children) VALUES
-('Каталог товаров', NULL, 1),
-('Мойки', 1, 1),
-('Ulgran', 2, 1),
-('Smth', 3, 0),
-('Smth', 3, 0),
-('Vigro Mramor', 3, 0),
-('Handmade', 2, 1),
-('Smth', 7, 0),
-('Smth', 7, 0),
-('Vigro Glass', 7, 0),
-('Фильтры', 1, 1),
-('Ulgran', 11, 1),
-('Smth', 12, 0),
-('Smth', 12, 0),
-('Vigro Mramor', 12, 0);
+INSERT INTO menu_items (name, parent_id) VALUES
+('Каталог товаров', NULL),
+('Мойки', 1),
+('Ulgran', 2),
+('Smth', 3),
+('Smth', 3),
+('Vigro Mramor', 3),
+('Handmade', 2),
+('Smth', 7),
+('Smth', 7),
+('Vigro Glass', 7),
+('Фильтры', 1),
+('Ulgran', 11),
+('Smth', 12),
+('Smth', 12),
+('Vigro Mramor', 12);
+
 
